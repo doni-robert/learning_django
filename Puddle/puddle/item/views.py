f6cceb3076878a0dbfc06304f62758344876ef67
doni-robert <irobertndungu@gmail.com>
2024-05-01 10:00:42 +0300

    chore: create add item form and implementation

--- Puddle/puddle/item/views.py
+++ Puddle/puddle/item/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+
 from .models import Item
+from .forms import NewItemForm
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -10,4 +13,23 @@ def detail(request, pk):
         'item': item,
         'related_items': related_items,
 
+    })
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid:
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = NewItemForm()
+
+    return render(request, 'form.html', {
+        'form': form,
+        'title': 'New Item',
     })
